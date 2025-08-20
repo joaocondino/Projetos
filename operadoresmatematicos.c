014793b7d7e7825aba823514e953f1523250a5d6
joaovictorcondino <202507319191@alunos.estacio.br>
2025-08-20 23:01:01 +0000

    Operadores matematicos

---
+++ operadoresmatematicos.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+  int a = 10;
+  int b = 5;
+
+  // Operadores aritméticos
+  int soma = a + b;
+  int subtracao = a - b;
+  int multiplicacao = a * b;
+  int divisao = a / b;
+
+  // Operadores de atribuição
+  //atribuição simples (=); atribuição com soma (+=); atribuição com subtração (-=); atribuição com divisão (/=)
+  a += 2;
+  b *= 3;
+
+  // Operadores de incremento (++) e decremento (--)
+  //São usados para aumentar ou diminuir o valor de uma variável em 1
+  
+  // Pré-incremento ex: ++a; Pós-incremento ex: a++
+  // Pré-decremento ex: --a; Pós-decremento ex: a--
+
+
+
+
+
+
+}
+
