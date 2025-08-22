b6a7c656b040e5ee411d208c87be59d92bbe9255
joaovictorcondino <202507319191@alunos.estacio.br>
2025-08-22 00:03:58 +0000

    novo

---
+++ serio.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int Populacao1;
+
+    printf("População: \n");
+    scanf("%d", &Populacao1);
+
+    printf("População: %d\n", Populacao1);
+
+    int Populacao2;
+
+    printf("População: \n");
+    scanf("%d", &Populacao2);
+
+    printf("População: %d\n", Populacao2);
+
+
+
+
+
+}
